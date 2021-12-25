c9983c8a2f45a9147cf7e442976f08571eccab88
Ajiharan <baskaranajiharan1243@gmail.com>
2021-12-25 19:51:58 +0530

    initialize project

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const filterFunc = (type = "greater", value, el) => {
+  if (type === "greater") return el > value;
+  if (type === "greaterEqual") return el >= value;
+  if (type === "smaller") return el < value;
+  if (type === "smallerEqual") return el <= value;
+  if (type === "doubleEqual") return el == value;
+  if (type === "TrippleEqual") return el === value;
+};
+
+const filterArray = function (arr, options = { value, condition: "greater" }) {
+  return arr.filter((el) => filterFunc(options.condition, options.value, el));
+};
+
+console.log(filterArray([1, 2, 5, 7], { value: 4, condition: "greaterEqual" }));
+
+export { filterArray };
